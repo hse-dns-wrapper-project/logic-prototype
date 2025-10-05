81bfbc5ddcfd6dc7c2ae38b0df7783b3aff4b629
fanbumbot <fanbumbot01@mail.ru>
2025-10-05 17:32:49 +0500

    POST request for add zone

--- src/app/main.py
+++ src/app/main.py
@@ -3,17 +3,15 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-"""
 @app.get("/")
 def read_root():
     html_content = "<h2>Hello METANIT.COM!</h2>"
     return HTMLResponse(content=html_content)
-"""
 
 from fastapi.responses import JSONResponse
 
-from .knot.transactions import open_socket, zone_transaction
-from .knot.dns_zone import get_zone, get_zones_list
+from app.knot.transactions import open_socket, zone_transaction
+from app.knot.dns_zone import get_zone, get_zones_list, add_zone
 
 @app.get("/zone")
 def get_all_zones():
@@ -21,9 +19,15 @@ def get_all_zones():
         zones = get_zones_list(ctl)
         return JSONResponse(zones)
 
-
 @app.get("/zone/{zone}")
 def get_zone_info(zone):
     with open_socket() as ctl:
         zones = get_zone(ctl, zone)
         return JSONResponse(zones)
+
+@app.post("/zone")
+def add_new_zone(
+    zone: str
+):
+    with open_socket() as ctl:
+        add_zone(ctl, zone)
